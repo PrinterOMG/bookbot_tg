58e178773451571b4bf42708f90ec87dd1abf6ad
PrinterOMG <PrinterOMG@ya.ru>
2022-01-14 22:50:54 +0400

    Add subscribes base methods

--- utils/db_api/subscribes_worker.py
+++ utils/db_api/subscribes_worker.py
@@ -1,4 +1,5 @@
 from .db_core import DatabaseCore
+import datetime
 
 
 class SubscribesWorker(DatabaseCore):
@@ -11,3 +12,26 @@ class SubscribesWorker(DatabaseCore):
             return records[0]
 
         return False
+
+    def create_subscribe_record(self, user_id, sub_type):
+        sql = f"SELECT duration FROM BookBotAdmin_subprices WHERE subPriceId={sub_type}"
+        sub_duration = self.send_query(sql)[0]["duration"]
+        start_date = datetime.date.today()
+        end_date = start_date + datetime.timedelta(days=30 * sub_duration)
+        sql = f"INSERT INTO BookBotAdmin_subscribes(startDate, endDate, subPriceId, user) VALUES({start_date}, {end_date}, {sub_type}, {user_id})"
+
+        self.send_query(sql)
+
+    def make_is_active_false(self, user_id):
+        sql = f"UPDATE BookBotAdmin_subscribes SET isActive=0 WHERE user={user_id}"
+
+        self.send_query(sql)
+
+    def update_subscribe_record(self, user_id, sub_type):
+        sql = f"SELECT duration FROM BookBotAdmin_subprices WHERE subPriceId={sub_type}"
+        sub_duration = self.send_query(sql)[0]["duration"]
+        start_date = datetime.date.today()
+        end_date = start_date + datetime.timedelta(days=30 * sub_duration)
+        sql = f"UPDATE BookBotAdmin_subscribes SET isActive=1, startDate={start_date}, endDate={end_date}, subPriceId={sub_type} WHERE user={user_id}"
+
+        self.send_query(sql)
