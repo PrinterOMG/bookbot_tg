89d725e13375199d0104e3e392baafcf1ee3fdb7
PrinterOMG <PrinterOMG@ya.ru>
2022-01-16 15:29:06 +0400

    * Test blocking

--- handlers/users/block.py
+++ handlers/users/block.py
@@ -5,4 +5,5 @@ from loader import dp
 
 @dp.my_chat_member_handler()
 async def block_user(my_chat_member: ChatMemberUpdated):
-    print(my_chat_member)
+    for el in my_chat_member:
+        print(el, my_chat_member[el])
