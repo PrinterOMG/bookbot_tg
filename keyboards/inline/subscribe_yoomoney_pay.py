e99fa523ded83f3c1751a207e86f458ca5e74122
cmdrBebop <kostyadevyatov@gmail.com>
2022-01-19 07:01:31 +0400

    Remove apple pay and google pay buttons

--- keyboards/inline/subscribe_yoomoney_pay.py
+++ keyboards/inline/subscribe_yoomoney_pay.py
@@ -11,12 +11,6 @@ async def get_yoomoney_pay_keyboard(user_id, amount, sub_id):
             InlineKeyboardButton("Bank card", callback_data=yoomoney_sub_callback.new("bank_card", amount, sub_id))
         ],
         [
-            InlineKeyboardButton("Apple Pay", callback_data=yoomoney_sub_callback.new("apple_pay", amount, sub_id))
-        ],
-        [
-            InlineKeyboardButton("Goggle Pay", callback_data=yoomoney_sub_callback.new("google_pay", amount, sub_id))
-        ],
-        [
             InlineKeyboardButton("YooMoney", callback_data=yoomoney_sub_callback.new("yoo_money", amount, sub_id))
         ],
         [
