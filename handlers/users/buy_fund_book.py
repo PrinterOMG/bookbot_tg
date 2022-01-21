d93474371ea9d1332c2795cd2f443af1fe623032
PrinterOMG <PrinterOMG@ya.ru>
2022-01-21 08:10:55 +0400

    * Fix fundraising

--- handlers/users/buy_fund_book.py
+++ handlers/users/buy_fund_book.py
@@ -5,6 +5,7 @@ from loader import dp, languages_worker, users_worker, books_worker
 from keyboards.inline.callbacks import buy_fund_book_callback
 from utils.check_book import check_book
 from .navigation import send_balance
+from .fundraising import send_fundraising_book_menu
 
 
 @dp.callback_query_handler(buy_fund_book_callback.filter())
@@ -19,9 +20,11 @@ async def buy_fund_book(call: CallbackQuery, callback_data: dict):
         await send_balance(call)
         return
 
-    await call.answer(text["buyBookOk"], show_alert=True)
     books_worker.add_payed_book_for_user(call.from_user.id, book_id)
-    await call.message.edit_text(text["fundraisingMenu"], reply_markup=await get_fundraising_keyboard(call.from_user.id))
     books_worker.add_to_collected_sum(book_id, price)
     users_worker.change_balance(call.from_user.id, f"-{price}")
+
     await check_book(book_id)
+
+    await call.answer(text["buyBookOk"], show_alert=True)
+    await send_fundraising_book_menu(call, {"id": book_id})
