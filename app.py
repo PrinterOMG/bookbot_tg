44f260a18900a819e35bebeb28b4f948d08210d6
cmdrBebop <kostyadevyatov@gmail.com>
2022-01-22 21:41:11 +0400

    test2

--- app.py
+++ app.py
@@ -34,6 +34,7 @@ async def on_startup(dispatcher):
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=False, on_startup=on_startup)
     print("executor")
+    executor.start_polling(dp, skip_updates=False, on_startup=on_startup)
+
 
