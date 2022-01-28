59ac4c0ecb53d31fc0c3a4c584f2de3c2af8b846
PrinterOMG <PrinterOMG@ya.ru>
2022-01-28 14:00:07 +0400

    * Fix close keyboard

--- keyboards/inline/close.py
+++ keyboards/inline/close.py
@@ -8,7 +8,9 @@ async def get_close_keyboard(user_id, final: int):
     text = languages_worker.get_text_on_user_language(user_id, "closeButton")
 
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        InlineKeyboardButton(text["closeButton"], callback_data=close_callback.new(final))
+        [
+            InlineKeyboardButton(text["closeButton"], callback_data=close_callback.new(final))
+        ]
     ])
 
     return keyboard
