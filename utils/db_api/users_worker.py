2927c92b8b3983259c5804c7260a553779792d9c
PrinterOMG <PrinterOMG@ya.ru>
2022-02-06 15:46:11 +0400

    * Fix

--- utils/db_api/users_worker.py
+++ utils/db_api/users_worker.py
@@ -94,7 +94,8 @@ class UsersWorker(DatabaseCore):
             sql_add.append(f"languageId_id={filters['languageId_id']}")
         if filters["subscribeStatus_id"]:
             sql_add.append(f'subscribeStatus_id={filters["subscribeStatus_id"]}')
-        sql_add.append(f'notEndPayment={filters["notEndPayment"]}')
+        if filters["notEndPayment_id"] != 3:
+            sql_add.append(f'notEndPayment={filters["notEndPayment_id"]}')
 
         sql_add.append(f'balance>={filters["balanceFrom"]}')
         if int(filters["balanceTo"]):
